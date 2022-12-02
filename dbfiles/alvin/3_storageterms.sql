1919f03fd4f0a7031782ba0379cdfb3f0aa6c9d7
olovm <olov.mckie@ub.uu.se>
2022-12-02 12:35:52 +0000

    adding alvin db files

---
+++ dbfiles/alvin/3_storageterms.sql
@@ -0,0 +1,4 @@
+COPY public.storageterm (recordtype, recordid, storagetermid, value, storagekey) FROM stdin;
+coraUser	coraUser:4412566252284358	userUserIdStorageTerm	alvinuser@cora.epc.ub.uu.se	userId
+coraUser	coraUser:5368244264733286	userUserIdStorageTerm	alvinguest@system.cora.uu.se	userId
+systemOneUser	151515	userUserIdStorageTerm	alvinuser@cora.epc.ub.uu.se	userId
