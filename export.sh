c8f9adadb29ba6c9183b29a8df0a590e273cddbf
lsu <lsu@b396dc4acf0d>
2022-12-14 13:22:12 +0000

    change export script from docker

--- export.sh
+++ export.sh
@@ -8,7 +8,7 @@ start(){
 	echo "Exporting data from docker "$dockerName
 	docker exec  -i $dockerName bash < runInsideDocker.sh
 	echo "Copying files to local export folder..."
-	docker cp $dockerName:/$baseDir .
+	docker cp $dockerName:/$baseDir/alvin .
 }
 
 start
