76b570ffd5c4bcf0b92c9bb50babd1669a022f25
DiVA Support <diva-support@ub.uu.se>
2024-04-10 16:51:03 +0200

    DbFiles updated ons 10 apr 2024 16:51:03 CEST

--- export.sh
+++ export.sh
@@ -9,6 +9,10 @@ start(){
 	docker exec  -i $dockerName bash < runInsideDocker.sh
 	echo "Copying files to local export folder..."
 	docker cp $dockerName:/$baseDir/alvin ./$baseDir
+	docker cp $dockerName:/$baseDir/alvinPreview ./$baseDir
+	docker cp $dockerName:/$baseDir/alvinTestSystem ./$baseDir
+	docker cp $dockerName:/$baseDir/alvinProduction ./$baseDir
+	docker cp $dockerName:/$baseDir/alvinData ./$baseDir
 }
 
 start
