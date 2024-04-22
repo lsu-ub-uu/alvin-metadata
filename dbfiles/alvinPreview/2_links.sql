23cd1b6a6fb81bc82142db9dd42a2b7b3122d53d
DiVA Support <diva-support@ub.uu.se>
2024-04-22 19:32:07 +0200

    DbFiles updated Mon Apr 22 07:32:07 PM CEST 2024

--- dbfiles/alvinPreview/2_links.sql
+++ dbfiles/alvinPreview/2_links.sql
@@ -1,1 +1,15 @@
 COPY public.link (fromtype, fromid, totype, toid) FROM stdin;
+system	alvinPreview	recordType	system
+system	alvinPreview	system	alvinPreview
+system	alvinPreview	text	alvinPreviewDefText
+system	alvinPreview	text	alvinPreviewText
+system	alvinPreview	user	151515
+system	alvinPreview	validationType	system
+text	alvinPreviewDefText	recordType	text
+text	alvinPreviewDefText	system	alvinPreview
+text	alvinPreviewDefText	user	151515
+text	alvinPreviewDefText	validationType	coraText
+text	alvinPreviewText	recordType	text
+text	alvinPreviewText	system	alvinPreview
+text	alvinPreviewText	user	151515
+text	alvinPreviewText	validationType	coraText
