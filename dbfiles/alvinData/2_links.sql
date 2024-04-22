7ce9b538c4fb87d3f4a3a2cca0bb80960b638af6
DiVA Support <diva-support@ub.uu.se>
2024-04-22 12:20:12 +0200

    DbFiles updated Mon Apr 22 12:20:12 PM CEST 2024

--- dbfiles/alvinData/2_links.sql
+++ dbfiles/alvinData/2_links.sql
@@ -1,1 +1,10 @@
 COPY public.link (fromtype, fromid, totype, toid) FROM stdin;
+alvin-organisation	1	recordType	alvin-organisation
+alvin-organisation	1	system	alvinData
+alvin-organisation	1	user	151515
+alvin-organisation	1	validationType	organisation
+alvin-record	22	alvin-location	3
+alvin-record	22	recordType	alvin-record
+alvin-record	22	system	alvinData
+alvin-record	22	user	151515
+alvin-record	22	validationType	record
