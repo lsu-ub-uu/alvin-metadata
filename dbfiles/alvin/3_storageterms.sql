c3913dfc486de2da5f37a4b113598692a4a0e152
DiVA Support <diva-support@ub.uu.se>
2024-05-27 10:00:10 +0200

    DbFiles updated Mon May 27 10:00:10 AM CEST 2024

--- dbfiles/alvin/3_storageterms.sql
+++ dbfiles/alvin/3_storageterms.sql
@@ -2,3 +2,16 @@ COPY public.storageterm (recordtype, recordid, storagetermid, value, storagekey)
 user	151515	userUserIdStorageTerm	alvinuser@cora.epc.ub.uu.se	userId
 user	coraUser:4412566252284358	userUserIdStorageTerm	alvinuser@cora.epc.ub.uu.se	userId
 user	coraUser:5368244264733286	userUserIdStorageTerm	alvinguest@system.cora.uu.se	userId
+user	user:15433956012260219	userUserIdStorageTerm	stand516@user.uu.se	userId
+user	user:15433970434951913	userUserIdStorageTerm	petth315@user.uu.se	userId
+user	user:15433978782831024	userUserIdStorageTerm	olfel499@user.uu.se	userId
+user	user:15433987769554915	userUserIdStorageTerm	palra847@user.uu.se	userId
+user	user:15433995147185923	userUserIdStorageTerm	danfl648@user.uu.se	userId
+user	user:15434004521008966	userUserIdStorageTerm	egisw290@user.uu.se	userId
+user	user:15434013255551660	userUserIdStorageTerm	evaag112@user.uu.se	userId
+user	user:15434020931125281	userUserIdStorageTerm	kenan399@user.uu.se	userId
+user	user:15434042351413533	userUserIdStorageTerm	perba577@user.uu.se	userId
+user	user:15434048568374750	userUserIdStorageTerm	marvi707@user.uu.se	userId
+user	user:15434055673621784	userUserIdStorageTerm	mican434@user.uu.se	userId
+user	user:15434062266597036	userUserIdStorageTerm	andma210@user.uu.se	userId
+user	user:15434069887791758	userUserIdStorageTerm	sarto903@user.uu.se	userId
