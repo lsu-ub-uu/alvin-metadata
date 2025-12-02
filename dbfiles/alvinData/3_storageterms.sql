91163a7ab2f53671b64f7410ee851dd06b932283
DiVA Support <diva-support@ub.uu.se>
2025-12-02 09:30:31 +0100

    DbFiles updated tis  2 dec 2025 09:30:31 CET

--- dbfiles/alvinData/3_storageterms.sql
+++ dbfiles/alvinData/3_storageterms.sql
@@ -1,2 +1,3 @@
 COPY public.storageterm (recordtype, recordid, storagetermid, value, storagekey) FROM stdin;
+user	user:2422721748622866	userLoginIdStorageTerm	ubstefan2@gmail.com	loginId
 user	user:5517150962293483	userLoginIdStorageTerm	alvinUser@uu.se	loginId
